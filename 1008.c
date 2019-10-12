a8c58b88e7a1bd7a1d23511523bcdcbb6babd465
zwm1114 <zwm1114@163.com>
2019-10-12 11:38:56 +0800

    kill bug in 1008

--- 1008.c
+++ 1008.c
@@ -1,27 +1,28 @@
 #include <stdio.h>
 
 int main(){
-	int arr[200]={0};
+	int A[200]={0};
 	int n,m,i,j,num;
 	int count = 0;
 	int k = 0;
 	scanf("%d %d",&n,&m);
+	m = m % n; 
 	int temp = m;
 	for(i=n;i<2*n;i++){
 		scanf("%d",&num);
- 		arr[i] = num;
+ 		A[i] = num;
 	}
    	for(j=n-m;m>0;m--){
-		arr[j] = arr[2*n-m];
-		arr[2*n-m]=0;
+		A[j] = A[2*n-m];
+		A[2*n-m]=0;
 		j++;
 	}
 	while(k<2*n-temp){
-		if(arr[k] != 0){
+		if(A[k] != 0){
 			if(count > 0){
 				printf(" ");
 			}
-			printf("%d",arr[k]);
+			printf("%d",A[k]);
 			count++;
 		}
 		k++;
