3246afda032871838f53b1a8d5c2325acdb565f5
zwm1114 <zwm1114@163.com>
2019-11-04 19:06:06 +0800

    :construction:

--- 1033.c
+++ 1033.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <ctype.h>
+
 
 int main(){
-	
+	char s[70];
+	int i = 0;
+	int j = 0;
+	int shift = 1;
+	int isAvailable;
+	scanf("%s",s);
+
+	while(s[i] != '\0'){
+		if(s[i] == '+'){
+			shift = 0;
+		}
+		i ++;
+	}
+	while((a = getchar()) != '\n'){
+		j = 0;
+		isAvailable = 1;
+		while(j < i){
+			if(a == s[j]){
+				isAvailable = 0;
+				break;
+			}
+			j++;
+		}
+		if(isAvailable && iswupper(a) && !shift){
+			putchar(a.towlower());
+		}
+		if(isAvailable){
+			putchar(a);
+		}
+	}
 	return 0;
 }
