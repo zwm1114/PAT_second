cac3e20155a4910b2bc8d835522f4383da9798ec
zwm1114 <zwm1114@163.com>
2019-11-11 23:16:11 +0800

    :construction:have not passed

--- 1020.c
+++ 1020.c
@@ -1,37 +1,66 @@
 #include <stdio.h>
+#define SIZE 1000
 
-/*找到最大的数的下标*/
-int max(float arr[]){
+typedef struct
+{
+	int inventory;
+	int charge;
+	
+}moones;
 
-}
+int max(moones [],int);
 
 int main(){
-	int n,r;
-	float arr_inventory[1000]={0};
-	float arr_charge[1000]={0};
-	float arr_unitPrice[1000]={0};
+	int n,demand;
 	int i = 0;
-
-	scanf("%d %d",&n,&r);
+	float sum = 0;
+	int maxIndex;
+	int per;
+	moones moon[SIZE];
+	scanf("%d%d",&n,&demand);
 
 	while(i<n){
-		scanf("%f",&arr_inventory[i]);
+		scanf("%d",&moon[i].inventory);
+		i++;
 	}
+
 	i = 0;
-	getchar();
+
 	while(i<n){
-		scanf("%f",&arr_charge[i]);
-	}
-	i=0;
-	while(i<n){
-		arr_unitPrice[i]=arr_charge[i]/arr_inventory[i];
+		scanf("%d",&moon[i].charge);
+		i++;
 	}
 
-	while(total < r){
-		
+	while(demand > 0){
+		maxIndex = max(moon,n);
+		if(moon[maxIndex].inventory <= demand){
+			per = moon[maxIndex].inventory;
+		}else{
+			per = demand;
+		}
+		demand -= per;
+		sum += ((float)moon[maxIndex].charge/moon[maxIndex].inventory) * per;
+		moon[maxIndex].inventory -=per;
 	}
-
-
+	
+	printf("%.2f",sum);
 
 	return 0;
+}
+
+//找出最大单价
+int max(moones moon [],int n){
+	int i = 0;
+	int maxIndex = 0;
+	float max = 0.0;
+	float unitCharge;
+	while(i<n){
+		unitCharge = (float)moon[i].charge/moon[i].inventory;
+		if(unitCharge > max && moon[i].inventory != 0){
+			max = unitCharge;
+			maxIndex = i;
+		}
+		i++;
+	}
+	return maxIndex;
 }
