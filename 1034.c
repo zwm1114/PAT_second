2edb967a7a3f30148991e7fce16678f633f8d36e
zwm1114 <zwm1114@163.com>
2019-12-04 18:06:07 +0800

    #

--- 1034.c
+++ 1034.c
@@ -2,7 +2,7 @@
 #include <string.h>
 void handleNum(long,long); 
 void calculate(long,long,long,long,char);
-long comMultiple(long,long);
+long comMultiple(long,long,int);
 
 int main(){
 	long a1,b1,a2,b2;
@@ -23,6 +23,7 @@ int main(){
 
 void handleNum(long a,long b){
 	long firPart;
+	long temp;
 	
 	if(a == 0 && b != 0){
 		printf("0 ");
@@ -32,8 +33,16 @@ void handleNum(long a,long b){
 		firPart = a/b;
 	}
 	
- 	if(a%b){
-		if(firPart){
+ 	if(a%b != 0){
+ 		temp = comMultiple(a,b,0);
+			if(temp != 1 && temp < 0){
+				a = a / (-temp);
+				b = b / (-temp);		
+			}else if(temp != 1 && temp >0){
+				a = a / temp;
+				b = b / temp;
+			} 
+		if(firPart != 0){
 			if(a < 0){
 				printf("(%ld %ld/%ld) ",firPart,-(a%b),b);
 			}else{
@@ -43,7 +52,11 @@ void handleNum(long a,long b){
 			printf("%ld/%ld ",a%b,b);
 		}	
 	}else{
-		printf("%ld ",firPart);
+		if(firPart < 0){
+			printf("(%ld) ",firPart);
+		}else{
+			printf("%ld ",firPart);
+		}		
 	}
 	
 }
@@ -51,7 +64,7 @@ void handleNum(long a,long b){
 void calculate(long a1,long b1,long a2,long b2,char a){
 	long comdenominator,elment1,elment2;
 	
-	comdenominator = comMultiple(b1,b2);
+	comdenominator = comMultiple(b1,b2,1);
 
 	elment1 = a1 * (comdenominator / b1);
 	elment2 = a2 * (comdenominator / b2);
@@ -63,34 +76,28 @@ void calculate(long a1,long b1,long a2,long b2,char a){
 		printf("= ");
 		handleNum(elment1+elment2,comdenominator);
 		printf("\n");
-	}
-
-	if(a == '-'){
+	}else if(a == '-'){
 		printf("- ");
 		handleNum(a2,b2);
 		printf("= ");
 		handleNum(elment1-elment2,comdenominator);
 		printf("\n");
-	}
-
-	if(a == '*'){
+	}else if(a == '*'){
 		printf("* ");
 		handleNum(a2,b2);
 		printf("= ");
-		handleNum(elment1*elment2,comdenominator);
+		handleNum(a1*a2,b1*b2);
 		printf("\n");
-	}
-
-	if(a == '/'){
+	}else if(a == '/'){
 		printf("/ ");
 		handleNum(a2,b2);
 		printf("= ");
-		handleNum(elment1/elment2,comdenominator);
+		handleNum(a1*b2,b2*a2);
 		printf("\n");
 	}
 }
 
-long comMultiple(long a,long b){
+long comMultiple(long a,long b,int sign){
 	long temp;
 	long x = a;
 	long y = b;
@@ -104,5 +111,13 @@ long comMultiple(long a,long b){
 		x = y;
 		y = temp;
 	}
-	return a * b / x;
+	
+	if(sign == 0){
+		return x;
+	}else{
+		return a * b / x;
+	}	
 }
+
+
+ 
