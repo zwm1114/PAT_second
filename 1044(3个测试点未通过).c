3e585735910b5872c303632ced9fad842b8f7bfb
zwm1114 <zwm1114@163.com>
2019-12-23 17:12:48 +0800

    :construction:

--- 1044(3个测试点未通过).c
+++ 1044(3个测试点未通过).c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#define SIZE 10
+void s_gets(char *);
 /*
   Author:zoe
   Date: 2019/12/22 22:14:42
@@ -12,20 +14,20 @@ struct mars
 };
 
 const struct mars low[] = {"tret","jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-const struct mars high[] = {"tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
+const struct mars high[] = {"0","tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
 
 int main()
 {
 	
 	int n;
-	char temp [10];
+	char temp [SIZE];
 	int length = 0;
 	int num = 0;
 	int j = 0;
-	scanf("%d",&n);
+	scanf("%d\n",&n);
 	
 	while(n--){
-		scanf("%s",temp);
+		s_gets(temp);
 		num = 0;
 		length = strlen(temp);
 		if(isdigit(temp[0])){
@@ -33,26 +35,26 @@ int main()
 				num = num * 10 + temp[j] - '0';
 			}
 			if(num < 13){
-				printf("%s\n",low[num]);
+				printf("%s\n",low[num].sign);
 			}else{
 				if(num%13){
-					printf("%s %s\n",high[num/13 - 1],low[num%13]);
+					printf("%s %s\n",high[num/13].sign,low[num%13].sign);
 				}else{
-					printf("%s\n",high[num/13 - 1]);
+					printf("%s\n",high[num/13].sign);
 				}
 			}
 		}else{
 			while(j < 13){
-				if(strstr(low[j].sign,temp) != NULL){
+				if(strstr(temp,low[j].sign) != NULL){
 					num = j; 
 					break;
 				}
 				j ++;
 			}
 			j = 0;
-			while(j < 12){
-				if(strstr(high[j].sign,temp) != NULL){
-					num = num + (j + 1) * 13; 
+			while(j < 13){
+				if(strstr(temp,high[j].sign) != NULL){
+					num = num + j * 13; 
 					break;
 				}
 				j ++;
@@ -63,3 +65,11 @@ int main()
     return 0;
 }
 
+void s_gets(char * str){
+	fgets(str,SIZE,stdin);
+	char * a = str;
+	while(*a != '\n'){
+		a ++;
+	}
+	*a = '\0';
+}
