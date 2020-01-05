8b414111b5152d9f86a14b98f2135ad15205ff52
zwm1114 <zwm1114@163.com>
2020-01-05 21:57:34 +0800

    :construction:

--- 1050.c
+++ 1050.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int cmp(int,int);
-void formatPut(int **,int *,int,int,int);
+void formatPut(int *,int *,int,int,int);
 
 int main(){
 	int N;
@@ -43,26 +43,34 @@ int cmp(int a,int b){
 	return b - a;
 }
 
-void formatPut(int **arr,int *num,int N,int m,int n){
+void formatPut(int *arr,int *num,int N,int m,int n){
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	while(i < N){
 		//left to right
 		while(k < (n - 1)){
-			arr[0][k ++] = num[i ++]; 
+//			arr[0][k ++] = num[i ++]; 
+			arr[0]++ = num[i ++];
+			k ++;
 		}
 		//top to bottom
 		while(j < (m - 1)){
-			arr[j ++][k] = num[i ++];
+//			arr[j ++][k] = num[i ++];
+			arr++ = num[i ++];
+			j ++; 
 		}
 		//right to left
 		while(k > 0){
-			arr[j][k --] = num[i ++];
+//			arr[j][k --] = num[i ++];
+			arr[j]-- = num[i ++];
+			k --;
 		}
 		//bottom to top
 		while(j > 0){
-			arr[j --][k] = num[i ++];
+//			arr[j --][k] = num[i ++];
+			arr -- = num[i ++];
+			j --;
 		}
 	}
 }
